3033e96ceb2c73c37507d2d7ade36e2052c78c0b
Mathias Frey <mathias.c.frey@gmail.com>
2023-04-30 23:29:03 +0200

    Added weeks.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from io import DEFAULT_BUFFER_SIZE
 import pandas as pd
 
 
@@ -29,9 +30,23 @@ polynomial = np.poly1d(coefficients)
 df['corr_coef'] = corr_coef
 df['reg_line'] = polynomial(df['t'])
 
+# Weekly
+df['Woche'] = pd.to_datetime(df['Datum']).dt.to_period('W')
+df_agg_w = df.groupby('Woche').agg({'t': 'mean', 'Verbrauch': 'sum'}).reset_index()
+df_agg_w.index=df_agg_w['Woche'].astype(str)
+# calculate correlation coefficient and regression line AGG
+corr_coef = df_agg_w['t'].corr(df_agg_w['Verbrauch'])
+coefficients = np.polyfit(df_agg_w['t'], df_agg_w['Verbrauch'], 3)
+polynomial = np.poly1d(coefficients)
+# add correlation coefficient and regression line to dataframe agg
+df_agg_w['corr_coef'] = corr_coef
+df_agg_w['reg_line'] = polynomial(df_agg_w['t'])
+
+
+
+# Monthly
 df['Monat'] = pd.to_datetime(df['Datum']).dt.to_period('M')
 df_agg = df.groupby('Monat').agg({'t': 'mean', 'Verbrauch': 'sum'}).reset_index()
-
 # needed for bar chart not able to plot period
 df_agg.index=df_agg['Monat'].astype(str)
 
@@ -47,10 +62,25 @@ df_agg['reg_line'] = polynomial(df_agg['t'])
 
 
 
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 5))
+fig, (axes, urxn) = plt.subplots(2, 2, figsize=(18, 5))
+
+
+ax5 = axes[0]
+# weekly
+ax5.bar(df_agg_w['Woche'].index, df_agg_w['Verbrauch'], color='blue')
+ax5.set_xlabel('Datum')
+ax5.set_ylabel(u'Verbrauch w\xf6 Summe')
+ax5.set_title(u'W\xf6chentliche Daten')
+ax6 = ax5.twinx()
+ax6.plot(df_agg_w['Woche'].index, df_agg_w['t'], color='red')
+ax6.set_ylabel('Temperatur Wien Durchschnitt', color='red')
+ax6.tick_params(axis='y', labelcolor='red')
+ax5.scatter(df_agg_w['Woche'].index, df_agg_w['reg_line'], color='green')
+
 
 
 # plot t on first y-axis
+ax1 = plt.subplot(2,1,2)
 ax1.bar(df['Datum'], df['Verbrauch'], color='blue')
 ax1.set_ylabel(u'Verbrauch t\xe4glich', color='blue')
 ax1.tick_params(axis='y', labelcolor='blue')
@@ -80,8 +110,11 @@ ax2.text(0.05, 0.95, textstr, transform=ax2.transAxes, fontsize=10,
 
 
 
+
+
+
 # plot aggregated data on second subplot
-ax3 = plt.subplot(1, 2, 2)
+ax3 = plt.subplot(2, 3, 3)
 ax3.bar(df_agg['Monat'].index, df_agg['Verbrauch'], color='blue')
 ax3.set_xlabel('Datum')
 ax3.set_ylabel('Verbrauch monatliche Summe')
@@ -91,12 +124,14 @@ ax4 = ax3.twinx()
 
 
 ax4.plot(df_agg['Monat'].index, df_agg['t'], color='red')
-ax4.set_ylabel('Temperatur Wien Durchschnit', color='red')
+ax4.set_ylabel('Temperatur Wien Durchschnitt', color='red')
 ax4.tick_params(axis='y', labelcolor='red')
 
 
 # plot regression line on second y-axis
 ax3.scatter(df_agg['Monat'].index, df_agg['reg_line'], color='green')
+
+
 
 
 # display the plot
